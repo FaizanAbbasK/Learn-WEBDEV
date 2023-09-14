ecb63b39aff22cc76a71b525efe887d8f1a82a71
Faizan Abbas <asadabbas116@gmail.com>
2023-09-14 20:28:45 +0500

    v1 (with LoginPage)

--- php/bsse-2.php
+++ php/bsse-2.php
@@ -3,7 +3,10 @@ $colors=array("red","blue","green");
 //echo $colors; wont work as it is an array.
 print_r($colors);
 var_dump($colors);
-
+$i=1;
+while($i<=5)
+  echo "the number is :  $i <br>"
+  $i++;
 
 
 ?>
